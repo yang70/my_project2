31c7b064a7e0dca59733e65e9583f6b6cd3b1eb8
Matthew Yang <yang70@gmail.com>
2015-09-01 16:04:22 -0700

    fixed long line in search_spec

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,7 +5,8 @@ describe "My search page" do # Minitest/spec - method takes name and bloc
     visit "http://google.com" # Capybara
     fill_in "q", with: "test" # Capybara
     click_on "Google Search" # Capybara
-    page.text.must_include "Test.com" # Capybara page.text string - .must_include from Minitest/spec - change
+    page.text.must_include "Test.com" # Capybara page.text string -
+                                      # .must_include from Minitest/spec
     page.text.must_include "Test - Wikipedia" # See above
   end
 end
